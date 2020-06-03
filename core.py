15752038bfe12248d471476d9a24e96d20b1f51e
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2020-06-03 21:26:17 +1000

    Add support to netcdfs without long_name property, add clause to broad try/except, started implementation of frame by frame generation of textures, and progress bar in mouse implemented when baking textures.

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ import bpy
 import os
 import xarray
 import numpy as np
+import glob
 
 from . import files_utils
 
@@ -15,8 +16,12 @@ class BlenderncEngine():
     def check_files_netcdf(self,file_path):
         """
         """
-        if os.path.isfile(file_path):
-            self.file_path = file_path
+        #file_folder = os.path.dirname(file_path)
+        if "*" in file_path:
+            self.file_path = glob.glob(file_path)
+            self.check_netcdf()
+        elif os.path.isfile(file_path):
+            self.file_path = [file_path]
             self.check_netcdf()
         else:
             raise NameError("File doesn't exist:",file_path)
@@ -25,22 +30,32 @@ class BlenderncEngine():
         """
         Check if file is a netcdf and contain at least one variable.
         """
-        extension = self.file_path.split('.')[-1]
-        if extension == ".nc":
-            self.load_netcd()
+        if len(self.file_path) == 1:
+            extension = self.file_path[0].split('.')[-1]
+            if extension == ".nc":
+                self.load_netcd()
+            else:
+                try:
+                    self.load_netcdf()
+                except:
+                    raise ValueError("File isn't a netCDF:",self.file_path)
         else:
-            try:
-                self.load_netcdf()
-            except:
-                raise ValueError("File isn't a netCDF:",self.file_path)
-
-    def load_netcdf(self):
+            extension = self.file_path[0].split('.')[-1]
+            if extension == ".nc":
+                self.load_netcd()
+            else:
+                try:
+                    self.load_netcdf()
+                except:
+                    raise ValueError("Files aren't netCDFs:",self.file_path)
+            
+    def load_netcdf(self,file=None):
         """
         """
-        if "*" in self.file_path:
-            self.dataset = xarray.open_mfdataset(self.file_path,decode_times=False)
+        if len(self.file_path) == 1 and file:
+            self.dataset = xarray.open_dataset(self.file_path,decode_times=False) 
         else:
-            self.dataset = xarray.open_dataset(self.file_path,decode_times=False)
+            self.dataset = xarray.open_mfdataset(self.file_path,decode_times=False,combine='by_coords')
 
     def netcdf_var(self):
         """
@@ -48,7 +63,10 @@ class BlenderncEngine():
         dimensions = [i for i in self.dataset.coords.dims.keys()]
         variable = list(self.dataset.variables.keys() - dimensions)
 
-        var_names = [(variable[ii], variable[ii], self.dataset[variable[ii]].long_name, "DISK_DRIVE", ii) for ii in range(len(variable))]
+        if self.dataset[variable[0]].long_name:
+            var_names = [(variable[ii], variable[ii], self.dataset[variable[ii]].long_name, "DISK_DRIVE", ii) for ii in range(len(variable))]
+        else:
+            var_names = [(variable[ii], variable[ii], variable[ii], "DISK_DRIVE", ii) for ii in range(len(variable))]
         return var_names
 
     def netcdf_values(self,selected_variable,active_resolution):
@@ -60,7 +78,8 @@ class BlenderncEngine():
 
         dict_var_shape = {ii:slice(0,variable[ii].size,self.resolution_steps(variable[ii].size,active_resolution))
                 for ii in variable.coords if 'time' not in ii}
-        print(active_resolution,selected_variable,dict_var_shape)
+        
+        print(selected_variable,dict_var_shape)
         # To Do: fix resolution implementation, perhaps a non linear coarsening
         variable_res = variable.isel(dict_var_shape)
         return variable_res
@@ -71,5 +90,4 @@ class BlenderncEngine():
         step = size * log_scale
         if step ==0:
             step = 1
-        print(log_scale,step,np.log10((size*res_interst/100)),np.log10(size))
         return int(step)
